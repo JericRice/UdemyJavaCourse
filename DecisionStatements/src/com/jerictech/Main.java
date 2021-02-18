80095d22e4fa0ffa37bd9b5b2825458053095230
Joe <jeric720@icloud.com>
2021-02-18 15:13:43 -0500

    Adding decision statement example

---
+++ DecisionStatements/src/com/jerictech/Main.java
@@ -0,0 +1,13 @@
+package com.jerictech;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("Not at alien");
+            System.out.println("And I am scared of aliens");
+        }
+    }
+}
