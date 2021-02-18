1d499284b246fc2a970aa87879dd3cc1af63a26f
Joe <jeric720@icloud.com>
2021-02-18 15:26:36 -0500

    Simplified logical operator

--- DecisionStatements/src/com/jerictech/Main.java
+++ DecisionStatements/src/com/jerictech/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         boolean isAlien = false;
-        if (isAlien == false) {
+        if (!isAlien) {
             System.out.println("Not at alien");
             System.out.println("And I am scared of aliens");
         }
