82be41cd088eca9a0b60c20dcdd5c3b281c2948a
Joe <jeric720@icloud.com>
2021-02-18 11:23:21 -0500

    Initial Commit

--- ByteShortIntLong/src/com/jerictech/Main.java
+++ ByteShortIntLong/src/com/jerictech/Main.java
@@ -1,8 +1,35 @@
 package com.jerictech;
 
+//Wrapper classes allow operations with a primitive
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Int minimum value = " + myMinIntValue);
+        System.out.println("Int maximum value = " + myMaxIntValue);
+        System.out.println("Busted max value = " + (myMaxIntValue + 1)); // overflow
+        System.out.println("Busted min value = " + (myMinIntValue - 1)); // underflow
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte minimum value = " + myMinByteValue);
+        System.out.println("Byte maximum value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short minimum value = " + myMinShortValue);
+        System.out.println("Short maximum value = " + myMaxShortValue);
+
+        long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long minimum value = " + myMinLongValue);
+        System.out.println("Long maximum value = " + myMaxLongValue);
+
     }
 }
