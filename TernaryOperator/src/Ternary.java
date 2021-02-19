255b3687e03fe9e5953801d50ec738f09f45384c
Joe <jeric720@icloud.com>
2021-02-18 19:29:49 -0500

    Adding ternary challenge

---
+++ TernaryOperator/src/Ternary.java
@@ -0,0 +1,12 @@
+public class Operato {
+
+    public static void main(String[] args) {
+
+
+
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+    }
+}
